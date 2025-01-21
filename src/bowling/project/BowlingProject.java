96f3f4e711d87e1d259df60fdcbfba6be8992012
Abdo <abdobuinees0@gmail.com>
2025-01-21 17:47:19 +0200

    a commit from git bash

--- src/bowling/project/BowlingProject.java
+++ src/bowling/project/BowlingProject.java
@@ -4,10 +4,8 @@ package bowling.project;
 public class BowlingProject {
 
     public static void main(String[] args) {
-        System.out.print("Hello New World!\n");
-                System.out.print("Hello New World!\n");
-
-        System.out.print("Hello New World!\n");
+        System.out.print("Hello my name is KiKi!\n");
+       
     }
     
 }
