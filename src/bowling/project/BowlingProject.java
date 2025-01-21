c3854641a6e06261d7c199b1e06c802879351ee6
abdoh <abdoh@Abdo-Hafez>
2025-01-21 16:28:46 +0200

    New Commit

--- src/bowling/project/BowlingProject.java
+++ src/bowling/project/BowlingProject.java
@@ -4,7 +4,7 @@ package bowling.project;
 public class BowlingProject {
 
     public static void main(String[] args) {
-        System.out.print("Hello World!\n");
+        System.out.print("Hello New World!\n");
     }
     
 }
